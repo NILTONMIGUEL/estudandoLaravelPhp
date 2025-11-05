9ff4d38c7b4804d9c55090b8d98bd6f941f8d4af
Nilton Miguel <niltonmiguel0@gmail.com>
2025-11-05 20:15:35 -0300

    nova aula ajustar

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email;
+    public $password;
+
+    public function login(){
+       $this->validate([
+        'email'=>'required|email',
+        'password'=>'required|min:6',
+       ]);
+
+       $auhenticated = Auth::attempt([
+            'email'=>$this->email,
+            'password'=>$this->password
+       ]);
+
+       if($auhenticated){
+            return redirect()->route('home');
+       }
+    }
+
     public function render()
     {
         return view('livewire.login');
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,16 +1,35 @@
 <div class="login-container">
-    <h2>Bem-vindo(a) ao <br><span style="color:#0d6efd;">ShopNow</span></h2>
-    <form action="#" method="POST">
+    <h2>Bem-vindo(a) ao <br><span style="color:#0d6efd;">{{config('app.name')}}</span></h2>
+    <form action="#" method="POST" wire:submit="login">
+      @csrf
       <div class="input-group">
-        <input type="email" name="email" placeholder="E-mail" required>
+        @error('email')
+        <input type="email" name="email" placeholder="E-mail" wire:model="email" required>
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
       </div>
       <div class="input-group">
-        <input type="password" name="password" placeholder="Senha" required>
+        <input type="password" name="password" placeholder="Senha" wire:model="password" required>
       </div>
       <button type="submit" class="login-btn">Entrar</button>
       <div class="links">
         <p><a href="#">Esqueceu a senha?</a></p>
         <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+      </div>
+      @error('email')
+      <div class="alert alert-danger">{{$message}}</div>
+      @enderror
+      @error('password')
+      <div class="alert alert-danger">{{$message}}</div>
+      @enderror
+
+
+      @if(session()->has('error'))
+        <div class="alert alert-danger">{{session()-get('error')}}</div>
+      @endif
+
+      <div class="text-center">
+        <small><a href="#">esqueceu a senha</a></small>
       </div>
     </form>
   </div>
