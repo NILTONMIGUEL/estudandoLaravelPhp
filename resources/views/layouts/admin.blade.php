819b752e29b0bdb93d4d61f61dfa089872b1e48f
Nilton Miguel <niltonmiguel0@gmail.com>
2025-11-19 20:58:54 -0300

    deixando as rotas com efeitos de ativas

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,17 +22,17 @@
       <h4 class="text-white mb-4">Admin</h4>
       <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-          <a href="{{route('painel')}}" class="nav-link {{request()->is('painel') ? 'active' : ''}}">
+          <a href="{{route('painel')}}" class="nav-link {{request()->routeIs('painel') ? 'active' : ''}}">
             <i class="bi bi-speedometer2 me-2"></i> Dashboard
           </a>
         </li>
         <li>
-          <a href="{{route('usuarios')}}" class="nav-link {{request()->is('usuarios') ? 'active' : ''}}">
+          <a href="{{route('usuarios')}}" class="nav-link {{request()->routeIs('usuarios') ? 'active' : ''}}">
             <i class="bi bi-people me-2"></i> Usuários
           </a>
         </li>
         <li>
-          <a href="{{route('produtos')}}" class="nav-link {{request()->is('produtos') ? 'active' : ''}}">
+          <a href="{{route('produtos')}}" class="nav-link {{request()->routeIs('produtos') ? 'active' : ''}}">
             <i class="bi bi-box-seam me-2"></i> Produtos
           </a>
         </li>
